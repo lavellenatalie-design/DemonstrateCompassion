12d1987ba681fec08756c9e3597f85c4da54cf0e
lavellenatalie-design <lavelle.natalie@gmail.com>
2026-03-05 14:17:41 -0500

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,119 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5jFKifAGc49":
+        Script1();
+        break;
+      case "6Z4APXBUH4I":
+        Script2();
+        break;
+      case "6M14SjDPSuj":
+        Script3();
+        break;
+      case "6lAlZNzKTFv":
+        Script4();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  const target = object('6P2kFGxMQLI');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5bw2pwddgJm';
+const teeterAmount = 10;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6OQMLOKk6rm');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6Od6WCrR6Su';
+const teeterAmount = 10;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script3 = function()
+{
+  const target = object('6DFTUXaS7IU');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6iShEtqqqi6';
+const teeterAmount = 10;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script4 = function()
+{
+  const target = object('6UAxSob5mh5');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6rEFzLqW4r7';
+const teeterAmount = 10;
+player.addForTriggers(
+id,
+target.animate(
+[ {rotate: '0deg' }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: `${teeterAmount}deg` }, 
+{rotate: `-${teeterAmount}deg` }, 
+{rotate: '0deg' } ]
+,
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
